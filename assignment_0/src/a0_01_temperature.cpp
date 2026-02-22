3aea7fdd13e8596804518381badff837820026e3
wangbo <2149343983@qq.com>
2026-02-22 19:20:45 +0800

    完成了a0_01任务的函数，学习了i/o流的基本使用

--- assignment_0/src/a0_01_temperature.cpp
+++ assignment_0/src/a0_01_temperature.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+#include <sstream>
+#include <string>
+#include <iomanip>
 #include "rm_a0/a0_01_temperature.hpp"
 
 int main() {
@@ -9,4 +11,28 @@ int main() {
   }
   std::cout << rm_a0::FormatFahrenheit(rm_a0::CelsiusToFahrenheit(celsius));
   return 0;
+}
+
+double rm_a0::CelsiusToFahrenheit(double celsius) {
+    return celsius * 1.8 + 32;
+}
+
+std::string rm_a0::FormatFahrenheit(double fahrenheit) {
+    std::ostringstream oss;
+    oss << std::fixed<<std::setprecision(2) <<fahrenheit;
+    return oss.str();
+}
+
+std::string SolveTemperature(const std::string &input, bool &ok) {
+    std::istringstream iss(input);
+    double celsius = 0.0;
+    if (iss >> celsius) {
+        ok = true;
+        double fahrenheit = rm_a0::CelsiusToFahrenheit(celsius);
+        std::string result = rm_a0::FormatFahrenheit(fahrenheit);
+        result+="\n";
+        return result;
+    }
+    ok = false;
+    return "";
 }
