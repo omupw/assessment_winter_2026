3ebc5a58445dd48f6d9ea9961b26287788842fd0
wangbo <2149343983@qq.com>
2026-03-08 17:03:03 +0800

    使用了分水岭算法解决了小球重叠问题

--- assignment_2/detector/src/main.cpp
+++ assignment_2/detector/src/main.cpp
@@ -2,15 +2,16 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+//g++ -o bin/detector src/main.cpp `pkg-config --cflags --libs opencv4`
 int main() {
     //导入图片并转换为hsv格式
-    cv::Mat image = cv::imread("../imgs/test1.jpg");
+    cv::Mat image = cv::imread("../imgs/test2.jpg");
     cv::Mat hsvImage;
     cv::cvtColor(image, hsvImage, cv::COLOR_BGR2HSV);
 
     //按阈值创建mask
     cv::Mat mask;
-    cv::Scalar lower_green = cv::Scalar(35,50,50);
+    cv::Scalar lower_green = cv::Scalar(40,65,65);
     cv::Scalar upper_green = cv::Scalar(85,255,255);
     cv::inRange(hsvImage, lower_green, upper_green, mask);
 
@@ -19,10 +20,35 @@ int main() {
     cv::morphologyEx(mask, mask, cv::MORPH_OPEN, kernel);
     cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, kernel);
 
+    //分水岭算法
+    cv::Mat dist;
+    cv::distanceTransform(mask, dist, cv::DIST_L2, 5);
+    double max_val;
+    cv::minMaxLoc(dist, nullptr, &max_val, nullptr, nullptr);
+    cv::Mat peaks;
+    cv::threshold(dist, peaks, 0.7*max_val, 255, cv::THRESH_BINARY);
+    peaks.convertTo(peaks, CV_8U);
+    cv::Mat markers;
+    cv::connectedComponents(peaks,markers,8,CV_32S);
+    markers = markers + 1;
+    cv::Mat unknown = mask -peaks;
+    markers.setTo(0,unknown ==255);
+    cv::watershed(image, markers);
+
     //找轮廓
     std::vector<std::vector<cv::Point>> contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(mask, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+    double min_label,max_label;
+    cv::minMaxLoc(markers, &min_label, &max_label);
+    for (int label = 2; label <= max_label; label++) {
+        cv::Mat ball_mask = (markers == label);
+        std::vector<std::vector<cv::Point>> contour;
+        cv::findContours(ball_mask, contour, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+        if (!contour.empty()) {
+            for (const auto& ct : contour) {
+                contours.push_back(ct);
+            }
+        }
+    }
 
     //画圆
     cv::Mat result = image.clone();
@@ -30,11 +56,15 @@ int main() {
     float radius;
     for (int i = 0; i < contours.size(); i++) {
         cv::minEnclosingCircle(contours[i], center, radius);
-        cv::circle(result, center, radius, cv::Scalar(0, 255, 0),2,cv::LINE_AA);
+        if (radius > 10) {
+            cv::circle(result, center, radius, cv::Scalar(0, 255, 0),2,cv::LINE_AA);
+        }
     }
     cv::imshow("result", result);
     cv::waitKey(0);
+    cv::imshow("mask", mask);
+    cv::waitKey(0);
     cv::destroyWindow("result");
-    cv::imwrite("../imgs/test1_result.jpg", result);
+    cv::imwrite("../imgs/test2_result.jpg", result);
     return 0;
 }
